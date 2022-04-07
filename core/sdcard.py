f96774244dd78f668769ecea9c02b39d8ad2236c
Andre Schepers <andre.schepers@yahoo.de>
2022-04-07 23:13:41 +0200

    Added pullup Pins to fix mounting error on reset, Removed Reset-Routine

--- core/sdcard.py
+++ core/sdcard.py
@@ -1,11 +1,11 @@
-from machine import Pin, SDCard, reset
-from os import mount, listdir
+from machine import Pin, SDCard
+from os import mount, umount, listdir
 
 ## SD-Card Modul ESP32 TTGO-T8 V1.7
 # Slot 1 (mosi=15, sck=14, dat1=4, dat2=12)
 # define Pins cs=13, miso=2
 
-def mount(slot=1, path="/sd", cs=13, miso=2, debug=False):
+def mounting(slot=1, width=1, path="/sd", cs=13, miso=2, debug=False):
     if not isinstance(path, str): raise ValueError('SDCard path must be a string')
     # NOTE: prevent Thonny-ManagementError if path is empty
     if len(path) == 0: raise ValueError('SDCard path is empty')
@@ -13,16 +13,22 @@ def mount(slot=1, path="/sd", cs=13, miso=2, debug=False):
     if path.replace('/', '') in listdir():
         print("SDCard path '{}' exists in directory".format(path))
         return False
-    print("mounting SDCard...")
+    print("\nmounting SDCard...")
     try:
         if debug: print('SDCard: slot={0}, Pins: cs={1}, miso={2}'.format(slot, cs, miso))
-        mount(SDCard(slot=slot, miso=Pin(miso), cs=Pin(cs)), path)
+        """
+        Note: Fix to mount SDCard with ESP32 TTGO-T8 V1.7
+        pull up the Pins: cs, miso
+        """
+        pullup_pin([miso, cs])
+        mount(SDCard(slot=slot, width=width, miso=Pin(miso), cs=Pin(cs)), path)
         print('SDCard mounted to path', path)
         return True
     except OSError as e:
         if debug: print('SDCard: errno', e)
-        if e.args[0] == 16:
-            if debug: print('SDCard: reset machine...')
-            reset()
         print("cannot mount SDCard")
         return False
+
+def pullup_pin(pins):
+    for pin in pins:
+        Pin(pin).init(-1, Pin.PULL_UP)
